db502ed3e031cc67bdb736e12ce1a54ba26df6f3
Nayan Agrawal <nayanagrawal995@gmail.com>
2018-01-18 20:01:12 +0530

    Added one print statement

--- src/test/java/com/test/autoset/quickstart/homepage/TC003_VerifyLoginWithDifferentRecords.java
+++ src/test/java/com/test/autoset/quickstart/homepage/TC003_VerifyLoginWithDifferentRecords.java
@@ -24,6 +24,7 @@ public class TC003_VerifyLoginWithDifferentRecords extends TestBase {
 		String[][] testRecords = getData("DataRead.xlsx", "NT");
 		System.out.println("Records -----------" + testRecords);
 		System.out.println("");
+		System.out.println("");
 		return testRecords;
 	}
 		
